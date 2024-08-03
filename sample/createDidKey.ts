8f0652fe40b66d662606ade1a1f5d8e2dd8f5dac
Kazuki Minakawa <kazuki.minakawa@funlab.jp>
2024-08-03 12:04:23 +0000

    did:key

--- sample/createDidKey.ts
+++ sample/createDidKey.ts
@@ -1,11 +1,23 @@
 import DidKey from '@kit/did/didKey'
 
 const main = async () => {
-  const key = new DidKey()
-  console.log('key:', key)
+  const key1 = new DidKey()
+  console.log('key1:', key1)
 
-  const jwk = key.getPrivateJwk()
-  console.log('jwk:', jwk)
+  const prv1 = key1.getPrivateJwk()
+  console.log('prv1:', prv1)
+
+  const pub1 = key1.getPublicJwk()
+  console.log('pub1:', pub1)
+
+  const key2 = DidKey.importDid(key1.did)
+  console.log('key2:', key2)
+
+  const prv2 = key2.getPrivateJwk()
+  console.log('prv2:', prv2)
+
+  const pub2 = key2.getPublicJwk()
+  console.log('pub2:', pub2)
 }
 
 main()
--- src/did/didKey.ts
+++ src/did/didKey.ts
@@ -5,15 +5,23 @@ export class DidKey {
   public keyPair: ec.KeyPair
   public did: string
 
-  constructor() {
-    const secp256k1 = new ec('secp256k1')
-    this.keyPair = secp256k1.genKeyPair()
+  constructor(keyPair?: ec.KeyPair) {
+    if (keyPair) {
+      this.keyPair = keyPair
+    } else {
+      const secp256k1 = new ec('secp256k1')
+      this.keyPair = secp256k1.genKeyPair()
+    }
+
     const pub = this.keyPair.getPublic()
     const xhex = pub.getX().toBuffer().toString('hex')
     const yhex = pub.getY().toBuffer().toString('hex')
 
-    const multicodec = `E701${parseInt(yhex.slice(-1), 16) % 2 == 1 ? '03' : '02'}`
-    this.did = `did:key:z${bs58.encode(Buffer.from(`${multicodec}${xhex}`, 'hex'))}`
+    const multicodec = 'e701'
+    const pubkey = `${parseInt(yhex.slice(-1), 16) % 2 == 1 ? '03' : '02'}${xhex}`
+    console.debug('hex:', multicodec, pubkey)
+
+    this.did = `did:key:z${bs58.encode(Buffer.from(`${multicodec}${pubkey}`, 'hex'))}`
   }
 
   getPublicJwk(): JsonWebKey {
@@ -26,12 +34,52 @@ export class DidKey {
     }
   }
 
-  getPrivateJwk(): JsonWebKey {
+  getPrivateJwk(): JsonWebKey | undefined {
+    const prv = this.keyPair.getPrivate()
+    if (!prv) {
+      return undefined
+    }
+
     const pubJwk = this.getPublicJwk()
     return {
       ...pubJwk,
-      d: this.keyPair.getPrivate().toBuffer().toString('base64url'),
+      d: prv.toBuffer().toString('base64url'),
     }
+  }
+
+  static importDid(did: string) {
+    const secp256k1 = new ec('secp256k1')
+    const parts = did.split(':')
+    if (parts.length < 3) {
+      throw new Error('did has invalid format')
+    }
+
+    const scheme = parts[0]
+    const method = parts[1]
+    const version = parts.length > 3 ? parts[2] : '1'
+    const multibaseValue = parts.length > 3 ? parts[3] : parts[2]
+
+    if (scheme != 'did') {
+      throw new Error('scheme must be "did"')
+    }
+
+    if (method != 'key') {
+      throw new Error('method must be "key"')
+    }
+
+    try {
+      Number(version)
+    } catch {
+      throw new Error('version must be number')
+    }
+
+    if (multibaseValue[0] != 'z') {
+      throw new Error('multibaseValue must start with z')
+    }
+
+    const data = bs58.decode(multibaseValue.substring(1))
+    const keyPair = secp256k1.keyFromPublic(data.slice(2))
+    return new DidKey(keyPair)
   }
 }
 
